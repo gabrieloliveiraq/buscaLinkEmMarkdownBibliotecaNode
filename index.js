92bc8c1297567e81958840666a2455fc95c7776d
gabriel <gabrieloliverdev@outlook.com>
2022-06-06 23:51:06 -0300

    create file system in index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-g
+import { default as chalk } from "chalk";
+import {default as fs} from "fs";
+// const chalk = require('fs');
+// const fs = require('fs');
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Falha ao localizar o arquivo'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        } else {
+        console.log(chalk.green(texto))};
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Principais Tecnologias/Recursos Utilizadas no Projeto
+
+- Utilização da lib Chalk via npm
+
+- Utilização da lib FileSystem (fs)
